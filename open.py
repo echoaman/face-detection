3cda99e1534c4e477f405231b569cab37ac9046f
Aman Pahurkar <amanpahurkar13@gmail.com>
2020-05-01 10:33:45 +0530

    first commit

---
+++ open.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+eyes_detect = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_detect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+LBFmodel = 'lbfmodel.yaml'
+
+landmark_detector  = cv2.face.createFacemarkLBF()
+landmark_detector.loadModel(LBFmodel)
+# while True:
+#     ret, img = cap.read()
+#     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#     face_list = face_detect.detectMultiScale(gray_img, 1.3, 5)
+
+#     for (x,y,w,h) in face_list:
+#         cv2.rectangle(img,(x,y), (x+w,y+h), (0,0,255), 1)
+#         roi = gray_img[y:y+h, x:x+w]
+#         roi_col = img[y:y+h, x:x+w]
+#         eye_list = eyes_detect.detectMultiScale(roi)
+
+#         for (eye_x, eye_y, eye_w, eye_h) in eye_list:
+#             cv2.rectangle(roi_col, (eye_x, eye_y), (eye_x + eye_w , eye_h + eye_y), (255,0,0) , 1) 
+    
+#     cv2.imshow('aman',img)
+
+#     k = cv2.waitKey(30) & 0xff
+#     if k == 27:
+#         break
+
+
+# cap.release()
+# cv2.destroyAllWindows()
+
+
+while(True):
+    # read webcam
+    _, frame = cap.read()
+
+    # convert frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces using the haarcascade classifier on the "grayscale image"
+    faces = face_detect.detectMultiScale(gray)    
+
+    for (x,y,w,d) in faces:
+        # Detect landmarks on "gray"
+        _, landmarks = landmark_detector.fit(gray, np.array(faces))
+
+        print(landmarks)
+        for landmark in landmarks:
+            for x,y in landmark[0]:
+                # display landmarks on "frame/image,"
+                # with blue colour in BGR and thickness 2
+                cv2.circle(frame, (x, y), 1, (255, 0, 0), 2)
+
+    # # save last instance of detected image
+    # cv2.imwrite('face-detect.jpg', frame)    
+    
+    # Show image
+    cv2.imshow("frame", frame)
+
+    # terminate the capture window
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
